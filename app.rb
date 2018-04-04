cb6f45bec517bbb031195ee69c428c93fbdcffa5
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-04 00:29:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,17 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      erb :show
+    end
     # code other routes/actions here
 
   end
